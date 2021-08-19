f33360678208e9af8614c95f31316ce354a28ed5
HitanshuIntern <84388209+HitanshuIntern@users.noreply.github.com>
2021-08-19 22:40:15 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from selenium import webdriver
+from time import sleep
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 400, height = 300)
+canvas1.pack()
+
+entry1 = tk.Entry (root) 
+canvas1.create_window(200, 140, window=entry1)
+
+def bot ():  
+    x1 = entry1.get()
+    
+    while True:
+        driver1 = webdriver.Chrome()
+        driver2= webdriver.Chrome()
+        driver3= webdriver.Chrome()
+        url = x1
+        sleep(2)
+        driver1.get(url)
+        driver2.get(url)
+        driver3.get(url)
+        sleep(600)
+
+        driver1.quit()
+        driver2.quit()
+        driver3.quit()
+    
+button1 = tk.Button(text='Submit', command=bot)
+canvas1.create_window(200, 180, window=button1)
+
+root.mainloop()
